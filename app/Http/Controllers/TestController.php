3af8e438b1c1ddd628a4fcd695742a8648078ccf
sunhao <sunhao@localhost.localdomain>
2020-03-10 00:46:35 +0000

    redis1

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class TestController extends Controller
+{
+    public function redis1(){
+        $key='aaa';
+        $val='bbb';
+        Redis::set($key,$val);
+        echo 'set成功';
+    }
+    public function redis2(){
+        $key='aaa';
+        $val=Redis::get($key);
+        echo 'val:'.$val;
+    }
+}
